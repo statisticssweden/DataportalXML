0c083ec9777d8e2c50eda0ded39369d4e4e3294c
Jonathan Erikson <jonatan.erikson@gmail.com>
2022-06-30 14:36:12 +0200

    Fix landing page url for PcAxis files

--- Source/Px.Rdf/RdfSettings.cs
+++ Source/Px.Rdf/RdfSettings.cs
@@ -9,16 +9,16 @@ namespace Px.Rdf
         public string BaseApiUrl; // Base url for api request
 
         public string PreferredLanguage; // language code 2 letters
-        public List<string> Languages;
+
+        public List<string> Languages; // Read from settings
 
         public string CatalogTitle;
         public string CatalogDescription;
 
         public string PublisherName;
         public string DBid;
-        public IFetcher Fetcher;
+        public IFetcher Fetcher; // Create depending on chosen database
         public string LandingPageUrl;
-
         public string License;
         public string ThemeMapping;
 
