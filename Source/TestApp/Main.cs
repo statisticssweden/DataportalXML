ea916a1f00b469331c5e3a12300efae0faf132a7
kevinguvenal <guvenalkevin@gmail.com>
2022-07-05 16:29:35 +0200

    fix #48 Documentation

--- Source/TestApp/Main.cs
+++ Source/TestApp/Main.cs
@@ -12,26 +12,19 @@ namespace TestApp
             {
                 BaseUri = "https://www.baseURI.se/", 
                 BaseApiUrl = "http://api.scb.se/OV0104/v1/doris/", 
-                PreferredLanguage = "sv", // For this example have English as preferredLang
+                PreferredLanguage = "sv",
                 Languages = new List<string>{"sv", "en"}, 
                 CatalogTitle = "SCB Tabeller", 
                 CatalogDescription = "-", 
                 PublisherName = "SCB", 
-                //DBid = @"C:\Temp\StatFin2018\StatFin\Menu.xml",//@"C:\Temp\Databases\Example\Menu.xml", @"C:\Temp\StatFin2018\StatFin\Menu.xml"
                 DBid = "ssd",
-                //Fetcher = new PcAxisFetcher(@"C:\Temp\StatFin2018\"),
                 Fetcher = new CNMMFetcher(),
                 LandingPageUrl = "http://www.statistikdatabasen.scb.se/goto/",
                 License = "http://creativecommons.org/publicdomain/zero/1.0/",
                 ThemeMapping = "C:/temp/TMapping.json"
             };
-            XML.writeToFile("../../../test.xml", settings);
-            //Console.ReadKey();
+            XML.WriteToFile("../../../test.xml", settings);
         }
     }
 
 }
-
-
-// @"C:\Temp\PxGit\PxWeb\PXWeb\Resources\PX\Databases\Example\Menu.xml", 
-// @"C:\Temp\Databases\Example\Menu.xml
